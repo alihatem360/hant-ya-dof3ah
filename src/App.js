32692deae7a5b0a0e9516149f8f03b12256edfaf
Ali Hatem <alihatemramadan5@gmail.com>
2024-04-10 15:17:04 +0200

    Remove unnecessary test files and unused code

--- src/App.js
+++ src/App.js
@@ -1,25 +1,50 @@
-import logo from './logo.svg';
-import './App.css';
+import React, { useState, useEffect } from "react";
+import mainImage from "./assets/main-image.webp";
+const CountdownTimer = () => {
+  const [timeRemaining, setTimeRemaining] = useState(calculateTimeRemaining());
 
-function App() {
+  useEffect(() => {
+    const timerInterval = setInterval(() => {
+      setTimeRemaining(calculateTimeRemaining());
+    }, 1000);
+
+    return () => {
+      clearInterval(timerInterval);
+    };
+  }, []);
+
+  function calculateTimeRemaining() {
+    const now = new Date().getTime();
+    const endDate = new Date("2025-03-01T00:00:00").getTime();
+    const timeRemaining = endDate - now;
+    return timeRemaining;
+  }
+
+  function formatTime(milliseconds) {
+    const totalDays = Math.floor(milliseconds / (1000 * 60 * 60 * 24));
+    const hours = Math.floor(
+      (milliseconds % (1000 * 60 * 60 * 24)) / (1000 * 60 * 60)
+    );
+    const minutes = Math.floor((milliseconds % (1000 * 60 * 60)) / (1000 * 60));
+    const seconds = Math.floor((milliseconds % (1000 * 60)) / 1000);
+
+    return `${totalDays} أيام ${hours} ساعات ${minutes} دقائق ${seconds} ثواني`;
+  }
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div id="countdown">
+      {timeRemaining >= 0 ? (
+        <div className="countdown-container">
+          <div>
+            <img src={mainImage} alt="main" className="main-image" />
+          </div>
+          <h2>{formatTime(timeRemaining)}</h2>
+        </div>
+      ) : (
+        <>انتهى العد التنازلي ✨</>
+      )}
     </div>
   );
-}
+};
 
-export default App;
+export default CountdownTimer;
